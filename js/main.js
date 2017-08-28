db56304cae9359502d65fbdcd7b9a720fa0ecea6
Jim Purcell <pur.jim@gmail.com>
2017-08-28 16:52:22 -0700

    Did some file clean up.

--- js/main.js
+++ js/main.js
@@ -8,50 +8,46 @@ var locations = [
           { name: 'Graze', fsid: '4db2325504374b3c49722dde'},
           { name: 'Gramercy Cellars', fsid: '4da0a2abb521224bd070e1ed'}
         ];
-
-var test = "testing123";
-
 //Initialize map
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    center: {lat: 46.0645809, lng: -118.3430209}
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: {lat: 46.0645809, lng: -118.3430209}
+    });
 var markers = [];
 //Add infowindow
-  var myInfoWindow = new google.maps.InfoWindow();
+var myInfoWindow = new google.maps.InfoWindow();
 //An array of favorite places with locations
-  var favPlaces = [
+var favPlaces = [
     {name: 'Brasserie Four', location: {lat: 46.066612, lng: -118.33807}},
     {name: 'Whitehouse-Crawford', location: {lat: 46.0686515, lng: -118.3421814}},
     {name: 'Saffron Mediterranean Kitchen', location: {lat: 46.0648235, lng: -118.3408598}},
     {name: 'Graze', location: {lat: 46.0677724, lng: -118.3366146}},
     {name: 'Gramercy Cellars', location: {lat: 46.0670125, lng: -118.3569582}}
-  ];
+    ];
   //Loop through favPlaces array to make a marker for each
-  for (i = 0; i<favPlaces.length; i++) {
+    for (i = 0; i<favPlaces.length; i++) {
     //Get lat lng
-    var position = favPlaces[i].location;
+      var position = favPlaces[i].location;
     //Get name
-    var title = favPlaces[i].name;
+      var title = favPlaces[i].name;
     //Create marker
-    var marker = new google.maps.Marker({
-    position: position,
-    map: map,
-    title: title,
-    animation: google.maps.Animation.DROP,
-    id: i
-    });
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
 
   //Push the created marker to the global marker array
-  markers.push(marker);
+    markers.push(marker);
 
 //Open the specific infowindow on click
-  marker.addListener('click', function() {
-    populateInfoWindow(this, myInfoWindow);
-  });
-
-}
+    marker.addListener('click', function() {
+      populateInfoWindow(this, myInfoWindow);
+    });
+  }
   function populateInfoWindow(marker, infowindow) {
 //Check if an infowindow is open, if not proceed
     if (window.marker !== marker) {
@@ -90,7 +86,7 @@ var markers = [];
       //Monitor search inputs
       self.userInput = ko.observable("");
 
-
+//Function to display in the list view if it contains search input
       self.searchResults = ko.computed(function() {
         var searchInput = self.userInput().toUpperCase();
         console.log(searchInput);
@@ -106,15 +102,9 @@ var markers = [];
             var cafeName = location.name;
               return (cafeName.toUpperCase().indexOf(searchInput) > -1);
           });
-
         }
-
       });
-
     };
-
-
-
 //});
 //AJAX request
 //$(document).ready(function(){
