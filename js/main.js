7d200579a63c6d9728ab21efc801c7e43de86eb6
Jim Purcell <pur.jim@gmail.com>
2017-09-11 17:14:37 -0700

    Moved var favPlaces out of map function.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 //Neigborhood Map Project
-var map;
 /*jshint loopfunc: true */
-
+var favPlaces = [{
+    name: 'Brasserie Four',
+    location: {
+      lat: 46.066612,
+      lng: -118.33807
+    }
+  },
+  {
+    name: 'Whitehouse-Crawford',
+    location: {
+      lat: 46.0686515,
+      lng: -118.3421814
+    }
+  },
+  {
+    name: 'Saffron Mediterranean Kitchen',
+    location: {
+      lat: 46.0648235,
+      lng: -118.3408598
+    }
+  },
+  {
+    name: 'Graze',
+    location: {
+      lat: 46.0677724,
+      lng: -118.3366146
+    }
+  },
+  {
+    name: 'Gramercy Cellars',
+    location: {
+      lat: 46.0670125,
+      lng: -118.3569582
+    }
+  }
+];
+var map;
 //Initialize map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -14,42 +49,7 @@ function initMap() {
     //Add infowindow
     var myInfoWindow = new google.maps.InfoWindow();
     //An array of favorite places with locations
-    var favPlaces = [{
-        name: 'Brasserie Four',
-        location: {
-          lat: 46.066612,
-          lng: -118.33807
-        }
-      },
-      {
-        name: 'Whitehouse-Crawford',
-        location: {
-          lat: 46.0686515,
-          lng: -118.3421814
-        }
-      },
-      {
-        name: 'Saffron Mediterranean Kitchen',
-        location: {
-          lat: 46.0648235,
-          lng: -118.3408598
-        }
-      },
-      {
-        name: 'Graze',
-        location: {
-          lat: 46.0677724,
-          lng: -118.3366146
-        }
-      },
-      {
-        name: 'Gramercy Cellars',
-        location: {
-          lat: 46.0670125,
-          lng: -118.3569582
-        }
-      }
-    ];
+
     //Loop through favPlaces array to make a marker for each
     for (i = 0; i<favPlaces.length; i++) {
       //Get lat lng
